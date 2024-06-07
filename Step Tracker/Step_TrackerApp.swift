e2d3af698f6267ead2d21385eaae5c1f16045f3e
Yernar Masujima <hikikomori@Yernars-MacBook-Pro.local>
2024-06-07 10:26:33 +0900

    Create new project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Yernar Masujima on 07.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
